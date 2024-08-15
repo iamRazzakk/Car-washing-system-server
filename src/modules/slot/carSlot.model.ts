2006ff8634531d43e05202288936f652b3c4eb11
iamRazzakk <rakibt23p@gmail.com>
2024-08-15 04:17:58 -0400

    create interface and model for car slot

---
+++ src/modules/slot/carSlot.model.ts
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose";
+import { TServiceSchedule } from "./carSlot.interface";
+
+const CarServiceSchema: Schema<TServiceSchedule> = new Schema({
+    service: {
+        type: Schema.Types.ObjectId,
+        ref: "CarService",
+        required: [true, "Object id is required"]
+    },
+    date: {
+        type: String,
+        required: [true, "Date is required"],
+        trim: true
+    },
+    startTime: {
+        type: String,
+        required: [true, "start Time is required"],
+        trim: true
+    },
+    endTime: {
+        type: String,
+        required: [true, "end time is required"],
+        trim: true
+    },
+    isBooked: {
+        type: String,
+        enum: ["available", "booked", "canceled"],
+        default: "available"
+    }
+
+}, { timestamps: true });
+export const carSlotBooking = model<TServiceSchedule>('carBookingSlot', CarServiceSchema);
