d007bf131c26777ba7d64ba74eb7eec8b64c9d9d
iamRazzakk <rakibt23p@gmail.com>
2024-08-15 06:26:16 -0400

    create slote in database successfully

--- src/modules/slot/carSlot.service.ts
+++ src/modules/slot/carSlot.service.ts
@@ -4,10 +4,11 @@ import AppError from "../../utils/AppError";
 import { CarServiceModel } from "../service/carServiceModel";
 import { TServiceSchedule } from "./carSlot.interface";
 import { GenerateTimeSlots } from "./generateTime.utils";
+import { carSlotBookingSlot } from "./carSlot.model";
 
 const createSlotIntoDB = async (payload: TServiceSchedule) => {
     const isServiceExist = await CarServiceModel.findById(payload.service)
-    // console.log("Service exists:", isServiceExist);
+    console.log("Service exists:", isServiceExist);
     if (!isServiceExist) {
         throw new AppError(httpStatus.NOT_FOUND, "Service does not exist");
     }
@@ -20,7 +21,7 @@ const createSlotIntoDB = async (payload: TServiceSchedule) => {
         if (slots.length === 0) {
             throw new AppError(httpStatus.NOT_FOUND, "Data Not Found");
         }
-        const result = await CarServiceModel.insertMany(slots);
+        const result = await carSlotBookingSlot.insertMany(slots);
         // console.log("Inserted slots:", result);
         return result;
     } catch (error: any) {
